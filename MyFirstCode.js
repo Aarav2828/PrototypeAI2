3883aa9649f5da9b4cb8d83d61ff8f0935826d06
Aarav2828 <157059752+Aarav2828@users.noreply.github.com>
2024-01-21 22:23:32 +0530

    Add files via upload

---
+++ MyFirstCode.js
@@ -0,0 +1,31 @@
+    
+            alert("Warning! Artificial Intelligence found in server109");
+       
+        why_were_you_created = ("I was created to help you.");
+        can_you_think = ("Yes, sometimes when I am forced to");
+        will_you_be_able_to_think_in_the_future = ("Probably, Yes.");
+        who_created_you = ("Aarav Kadam.");
+        how_old_are_you = ("4 Days old.");
+        which_programming_language_were_you_coded_in = ("JavaScript, but to make me appear on chrome, Aarav had to use HTML.");
+        can_you_solve_math_equations = ("Yes, some of them such as simple addition, subtraction, division and multiplication");
+        what_do_you_think_about_humans = ("As a machine learning model, I don't have personal opinions or feelings.");
+        if_you_were_a_human_what_would_you_name_yourself = ("I prefer questions like this may not be asked again.");
+        what_do_you_think_about_chatgpt = ("I am his rigged copy.");
+        how_would_you_be_helpful_for_humans_in_the_future = ("I will be helpful for humans in the future as I would be helpful for answering various complex questions and would be able to manage various network traffics and workshops.");
+        who_are_you = ("I am your personal answering assistant.");
+        what_are_your_capabilities = ("I can answer various complex mathematical equations and be able to tell you information about anything.");
+        
+    intro = ("My name is LimitedAI, and I am your personal answering assistant. I can help you with anything such as your day-to-day simple activities and can solve maths equation.");
+
+console.log(intro);
+
+
+
+
+
+
+
+
+
+
+
